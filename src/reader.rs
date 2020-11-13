4657f9721ea7e395c25f67e6a7afa219b6620ae8
Shell Chen <me@sorz.org>
2020-11-13 16:31:20 +0800

    Initial commit

---
+++ src/reader.rs
@@ -0,0 +1,127 @@
+use std::{
+    collections::HashSet,
+    io::{Read, Result, ErrorKind, Write},
+    iter::FromIterator,
+    marker::PhantomData,
+    convert::TryInto,
+    cmp::min,
+};
+use byteorder::{ReadBytesExt, BigEndian};
+
+pub mod states {
+    pub struct Ready;
+    pub struct Accepted;
+    pub struct Started;
+
+    pub trait BeforeStart {}
+    impl BeforeStart for Ready {}
+    impl BeforeStart for Accepted {}
+}
+
+pub struct FstrmReader<R, S> {
+    reader: R,
+    state: PhantomData<S>,
+}
+
+impl<R, S> FstrmReader<R, S> {
+    pub fn new(reader: R) -> FstrmReader<R, states::Ready> {
+        FstrmReader {
+            reader,
+            state: PhantomData,
+        }
+    }
+
+    pub fn into_inner(self) -> R {
+        self.reader
+    }
+}
+
+impl<R: Read, S: states::BeforeStart> FstrmReader<R, S> {
+    /// Read the START frame.
+    pub fn start(&mut self) -> Result<FstrmReader<R, states::Started>> {
+        unimplemented!()
+    }
+}
+
+impl<R: Read + Write> FstrmReader<R, states::Ready> {
+    /// Read the READY frame then reply with ACCEPT, if content types are matched.
+    /// Set allowed_content_types as empty to allow any content type.
+    pub fn accept<'a, T, S: 'a>(&mut self, allowed_content_types: T) -> Result<FstrmReader<R, states::Accepted>>
+    where
+        T: IntoIterator<Item = &'a S>,
+        S: AsRef<str>,
+    {
+        let allowed_types: HashSet<&str> =
+            HashSet::from_iter(allowed_content_types.into_iter().map(|s| s.as_ref()));
+
+        unimplemented!()
+    }
+
+}
+
+impl<R: Read + Write> FstrmReader<R, states::Accepted> {
+    /// Write FINISH frame, return the inner reader.
+    pub fn finish(&self) -> Result<R> {
+        unimplemented!()
+    }
+}
+
+impl<R: Read> FstrmReader<R, states::Started> {
+    pub fn read_frame(&mut self) -> Result<Option<Frame<R>>> {
+        let len = self.reader.read_u32::<BigEndian>()?;
+        if len == 0 {
+            // TODO: handle control frame
+            Ok(None)
+        } else {
+            Ok(Some(Frame::new(&mut self.reader, len)))
+        }
+    }
+}
+
+pub struct Frame<'a, R> {
+    reader: &'a mut R,
+    size: usize,
+    pos: usize,
+}
+
+impl<'a, R> Frame<'a, R> {
+    fn new(reader: &'a mut R, size: u32) -> Self {
+        Self {
+            reader, size: size.try_into().unwrap(), pos: 0
+        }
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn remaining(&self) -> usize {
+        self.size - self.pos
+    }
+}
+
+impl<'a, R: Read> Read for Frame<'a, R> {
+
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let max_len = min(buf.len(), self.remaining());
+        let n = self.reader.read(&mut buf[..max_len])?;
+        self.pos += n;
+        if n == 0 && self.remaining() != 0 {
+            Err(ErrorKind::UnexpectedEof.into())
+        } else {
+            Ok(n)
+        }
+    }
+}
+
+pub struct Frames<'a, R> {
+    reader: &'a mut FstrmReader<R, states::Started>,
+}
+
+impl<'a, R: Read> Iterator for Frames<'a, R> {
+    type Item = Result<Frame<'a, R>>;
+
+    fn next(&mut self) -> Option<Result<Frame<'a, R>>> {
+        self.reader.read_frame().transpose()
+    }
+}
